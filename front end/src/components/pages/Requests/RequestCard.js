4a1691a0e11c03a53152de400ddb1540c17d7585
AmrHendy <amr.m.hendy@gmail.com>
2019-05-11 15:27:37 +0200

    Added initial request page ui

--- front end/src/components/pages/Requests/RequestCard.js
+++ front end/src/components/pages/Requests/RequestCard.js
@@ -3,57 +3,80 @@ import React from "react";
 import './RequestCard.css'
 
 class RequestCard extends React.Component {
-  constructor(props) {
-    super(props);
-  }
+    constructor(props) {
+        super(props);
+        this.viewResume = this.viewResume.bind(this);
+        this.confirmRequest = this.confirmRequest.bind(this);
+        this.rejectRequest = this.rejectRequest.bind(this);
+    }
 
-  render() {
-    return (        
-        <div class="w3-row w3-card-2 w3-round-xlarge requestCard-comp1">
+    viewResume(e) {
+        e.preventDefault();
+        window.open(this.props.request.sender.resumeLink);
+    }
 
-            <div class="w3-col w3-center requestCard-comp2">
-                <img class="w3-card-2 requestCard-comp3" src="profile.png"/>
-                <div class="w3-large requestCard-comp4">Amr Hendi</div>
+    confirmRequest(e){
+        e.preventDefault();
+        const {id} = this.props.request.info;
+        this.props.changeRequestStatus(id, 'confirmed');
+    }
+
+    rejectRequest(e){
+        e.preventDefault();
+        const {id} = this.props.request.info;
+        this.props.changeRequestStatus(id, 'rejected');
+    }
+
+    render() {
+        const {profileImage, firstName, lastName, resumeLink} = this.props.request.sender;
+        const {position, company, city, country} = this.props.request.info;
+
+        return (        
+            <div class="w3-row w3-card-2 w3-round-xlarge requestCard-comp1">
+                <div class="w3-col w3-center requestCard-comp2">
+                    <img class="w3-card-2 requestCard-comp3" src={profileImage}/>
+                    <div class="w3-large requestCard-comp4">{firstName} {lastName}</div>
+                </div>
+
+                <div class="w3-col w3-large requestCard-comp5">
+                    <div class="w3-row">
+                        <div class="w3-col requestCard-comp6">Position</div>
+                        <div class="w3-col requestCard-comp7"> : </div>
+                        <div class="w3-col requestCard-comp8">{position}</div>
+                    </div>
+                    <div class="w3-row">
+                        <div class="w3-col requestCard-comp6">Company</div>
+                        <div class="w3-col requestCard-comp7"> : </div>
+                        <div class="w3-col requestCard-comp8">{company}</div>
+                    </div>
+                    <div class="w3-row">
+                        <div class="w3-col requestCard-comp6">Office</div>
+                        <div class="w3-col requestCard-comp7"> : </div>
+                        <div class="w3-col requestCard-comp8">{city}, {country}</div>
+                    </div>
+                </div>
+
+                <div class="w3-col w3-small requestCard-comp9">
+                    <div>
+                        <button class="w3-btn w3-teal w3-round-large requestCard-comp10" onClick={this.viewResume}>
+                            view resume
+                            {resumeLink}
+                        </button>
+                    </div>
+                    <div>
+                        <button class="w3-btn w3-green w3-round-large requestCard-comp10" onClick={this.confirmRequest}>
+                            confirm application
+                        </button>
+                    </div>
+                    <div>
+                        <button class="w3-btn w3-red w3-round-large requestCard-comp11" onClick={this.rejectRequest}>
+                            reject application
+                        </button>
+                    </div>
+                </div>
             </div>
-
-            <div class="w3-col w3-large requestCard-comp5">
-                <div class="w3-row">
-                    <div class="w3-col requestCard-comp6">Position</div>
-                    <div class="w3-col requestCard-comp7"> : </div>
-                    <div class="w3-col requestCard-comp8">full-stack developer</div>
-                </div>
-                <div class="w3-row">
-                    <div class="w3-col requestCard-comp6">Company</div>
-                    <div class="w3-col requestCard-comp7"> : </div>
-                    <div class="w3-col requestCard-comp8">Google</div>
-                </div>
-                <div class="w3-row">
-                    <div class="w3-col requestCard-comp6">Office</div>
-                    <div class="w3-col requestCard-comp7"> : </div>
-                    <div class="w3-col requestCard-comp8">Menlo Park</div>
-                </div>
-            </div>
-
-            <div class="w3-col w3-small requestCard-comp9">
-                <div>
-                    <button class="w3-btn w3-teal w3-round-large requestCard-comp10">
-                    view resume
-                    </button>
-                </div>
-                <div>
-                    <button class="w3-btn w3-green w3-round-large requestCard-comp10">
-                    confirm application
-                    </button>
-                </div>
-                <div>
-                    <button class="w3-btn w3-red w3-round-large requestCard-comp11">
-                    reject application
-                    </button>
-                </div>
-            </div>
-        </div>
-    );
-  }
+        );
+    }
 }
 
 export default RequestCard;
--- front end/src/components/pages/Requests/RequestNav.js
+++ front end/src/components/pages/Requests/RequestNav.js
@@ -10,10 +10,10 @@ class RequestNav extends React.Component {
   render() {
     return (
     <div className="w3-col w3-card-2 w3-round-large w3-large w3-border requestNav-comp1">
-        <div className="w3-hover-grey requestNav-comp2">
+        <div className="w3-hover-grey requestNav-comp2" onClick={this.props.showSentRequests}>
             Sent Requests
         </div>
-        <div className="w3-hover-grey w3-teal requestNav-comp2">
+        <div className="w3-hover-grey w3-teal requestNav-comp2" onClick={this.props.ReceivedSentRequests}>
             Received Requests
         </div>
     </div>
